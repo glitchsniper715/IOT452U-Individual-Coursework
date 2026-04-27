34552e3ee0512c4389c3f9d5713a6ece5410432a
Sajni Mei Amin <ec25741@qmul.ac.uk>
2026-04-27 14:51:11 +0100

    feat(exception): all domain exception classes created - closes #12

--- src/main/java/com/digitalid/exception/UnauthorisedActionException.java
+++ src/main/java/com/digitalid/exception/UnauthorisedActionException.java
@@ -1,6 +1,7 @@
 package com.digitalid.exception;
 
 public class UnauthorisedActionException extends RuntimeException {
+
     public UnauthorisedActionException(String message) {
         super(message);
     }
--- src/main/java/com/digitalid/exception/ValidationException.java
+++ src/main/java/com/digitalid/exception/ValidationException.java
@@ -1,6 +1,7 @@
 package com.digitalid.exception;
 
 public class ValidationException extends RuntimeException {
+
     public ValidationException(String message) {
         super(message);
     }
